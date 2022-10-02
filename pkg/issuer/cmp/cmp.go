564e986b1aa76fee2b6bd5b5a058a332ad109ea5
Sathyanarayanan Saravanamuthu <sathya.chozhanaadan@gmail.com>
2022-10-02 10:37:48 +0530

    More changes

--- pkg/issuer/cmp/cmp.go
+++ pkg/issuer/cmp/cmp.go
@@ -3,6 +3,8 @@ package cmp
 import (
 	"fmt"
 
+	corelisters "k8s.io/client-go/listers/core/v1"
+
 	apiutil "github.com/cert-manager/cert-manager/pkg/api/util"
 	v1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"github.com/cert-manager/cert-manager/pkg/controller"
@@ -10,11 +12,21 @@ import (
 )
 
 type Cmp struct {
+	*controller.Context
+	issuer v1.GenericIssuer
+
+	secretsLister corelisters.SecretLister
 }
 
 func New(ctx *controller.Context, issuer v1.GenericIssuer) (issuer.Interface, error) {
 	fmt.Println("## CMP New called")
-	return &Cmp{}, nil
+	secretsLister := ctx.KubeSharedInformerFactory.Core().V1().Secrets().Lister()
+
+	return &Cmp{
+		Context:       ctx,
+		issuer:        issuer,
+		secretsLister: secretsLister,
+	}, nil
 }
 
 // Register this Issuer with the issuer factory
--- pkg/issuer/cmp/setup.go
+++ pkg/issuer/cmp/setup.go
@@ -3,9 +3,18 @@ package cmp
 import (
 	"context"
 	"fmt"
+
+	apiutil "github.com/cert-manager/cert-manager/pkg/api/util"
+	v1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 )
 
-func (a *Cmp) Setup(ctx context.Context) error {
+const (
+	successReady = "IsReady"
+)
+
+func (c *Cmp) Setup(ctx context.Context) error {
 	fmt.Println("## CMP Setup called")
+	apiutil.SetIssuerCondition(c.issuer, c.issuer.GetGeneration(), v1.IssuerConditionReady, cmmeta.ConditionTrue, successReady, "")
 	return nil
 }
